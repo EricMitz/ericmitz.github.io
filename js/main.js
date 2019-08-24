204872afb2da14cfe7e916e46a1d3ff5e8d12344
EricMitz <54458754+EricMitz@users.noreply.github.com>
2019-08-24 04:50:23 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,7 +23,8 @@ function processData(allText) {
 
             var tarr = [];
             for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
+                //tarr.push(headers[j]+":"+data[j]);
+				tarr.push(data[j]);
             }
             lines.push(tarr);
         }

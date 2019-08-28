3098246ef26c42cc289ef930fd6e09963d75de9f
EricMitz <54458754+EricMitz@users.noreply.github.com>
2019-08-28 00:28:54 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,7 +10,36 @@ $(document).ready(function() {
         dataType: "text",
         success: function(data) {processData(data);}
      });
+	 
+	 $.ajax({
+        type: "GET",
+        url: "timelines.csv",
+        dataType: "text",
+        success: function(data) {processDataFollowers(data);}
+     });
 });
+
+function processDataFollowers(allText) {
+	var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<headers.length; j++) {
+                //tarr.push(headers[j]+":"+data[j]);
+				tarr.push(data[j]);
+            }
+            lines.push(tarr);
+        }
+    }
+	
+	console.log(lines[0]);
+	
+}
 
 function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
